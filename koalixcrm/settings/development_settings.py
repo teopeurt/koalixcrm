16162792ef909cdebeaf79a7747a5d757d71961c
Simon Riedener <simon.riedener@koalix.ch>
2017-08-21 22:28:04 +0200

    [no-issue] Adapted settings files.

--- koalixcrm/settings/development_settings.py
+++ koalixcrm/settings/development_settings.py
@@ -1,1 +1,12 @@
 from .base_settings import *
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
