8764dbbccf8e70ec408f1d0e0fd668bb5a59e2f2
Simon Riedener <simon.riedener@koalix.ch>
2017-08-22 10:56:29 +0200

    [no-issue] Adapted wsgi files.

--- koalixcrm/wsgi.py
+++ koalixcrm/wsgi.py
@@ -11,4 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "koalixcrm.settings.heroku_settings")
+
 application = get_wsgi_application()
